a4ee4c36ac3a7e8b370cdb3968641416ab135b39
Gilles CRIELOUE <gilles_crieloue@ext.connect-tech.sncf>
2024-01-23 15:02:08 +0100

    dev

--- scripts/check_enable.js
+++ scripts/check_enable.js
@@ -2,8 +2,8 @@
   const article = document.querySelector("article");
 
   if (article) {
-    chrome.runtime.sendMessage({ message: "enable" });
+    chrome.runtime.sendMessage({ message: "enable" }, function (response) {});
   } else {
-    chrome.runtime.sendMessage({ message: "disable" });
+    chrome.runtime.sendMessage({ message: "disable" }, function (response) {});
   }
 }
